39ba133e80d5261242b567924c46ddf99ad145f5
Tao-Kim <taog397@gmail.com>
2020-07-19 12:51:05 +0900

    근태 현황 리포트 UI작업 완료

--- app/src/main/java/com/gbridge/etners/ui/admin/DailyAttendanceReportFragment.java
+++ app/src/main/java/com/gbridge/etners/ui/admin/DailyAttendanceReportFragment.java
@@ -3,6 +3,7 @@ package com.gbridge.etners.ui.admin;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -14,6 +15,8 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.gbridge.etners.R;
 import com.gbridge.etners.data.DailyAttendanceReportItem;
@@ -21,16 +24,21 @@ import com.gbridge.etners.data.EmployeeAttendanceItem;
 import com.gbridge.etners.ui.admin.adapter.DailyAttendanceReportAdapter;
 import com.gbridge.etners.ui.admin.adapter.EmployeeAttendanceManagementAdapter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 
 public class DailyAttendanceReportFragment extends Fragment {
 
 
     private static final String TAG = "DAR";
-
-
+    private RecyclerView recyclerView;
+    private CalendarView calendarView;
+    private TextView tvDate;
+    private DailyAttendanceReportAdapter myAdapter;
     public DailyAttendanceReportFragment() {
     }
 
@@ -51,9 +59,15 @@ public class DailyAttendanceReportFragment extends Fragment {
         Log.d(TAG, "onCreateView: RUN");
         View view = inflater.inflate(R.layout.fragment_daily_attendance_report, container, false);
 
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_daily_attendance_report);
+        calendarView = (CalendarView) view.findViewById(R.id.calendar_view_daily_attendance_report);
+        tvDate = (TextView) view.findViewById(R.id.tv_daily_attendance_report_date);
+
         // Set actionbar
         initActionBar();
-        initRecyclerView(view);
+        initRecyclerView();
+        initCalendarView();
 
         return view;
     }
@@ -67,15 +81,55 @@ public class DailyAttendanceReportFragment extends Fragment {
 
     }
 
-    private void initRecyclerView(View view){
+    private void initRecyclerView() {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL);
         dividerItemDecoration.setDrawable(getContext().getResources().getDrawable(R.drawable.divider_admin));
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_daily_attendance_report);
         recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
         recyclerView.setNestedScrollingEnabled(false);
-        recyclerView.setAdapter(new DailyAttendanceReportAdapter(getTestData()));
+        myAdapter = new DailyAttendanceReportAdapter();
+        recyclerView.setAdapter(myAdapter);
+
+        /*******************************************************************************
+
+         TODO: 작업1 - 서버에서 데이터 불러오는 작업 필요 By 날짜 (REST API 개발중)
+         초기 뷰 생성시 오늘 날짜로 rest api 호출 및
+         updateRecyclerView에 LIST 전달
+
+         ******************************************************************************/
+        updateRecyclerView(getTestData());
+    }
+
+    private void initCalendarView() {
+        Long currentTime = calendarView.getDate();
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        String currentYear = yearFormat.format(currentTime);
+        String currentMonth = monthFormat.format(currentTime);
+        String currentDay = dayFormat.format(currentTime);
+        tvDate.setText(String.format("%s년 %s월 %s일", currentYear, currentMonth, currentDay));
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                tvDate.setText(String.format(Locale.KOREA, "%d년 %d월 %d일", year, month + 1, dayOfMonth));
+
+                /*******************************************************************************
+
+                 TODO: 작업1 - 서버에서 데이터 불러오는 작업 필요 By 날짜 (REST API 개발중)
+                 날짜 선택시 REST API 호출하고
+                 updateRecyclerView에 LIST 넘겨야 함
+
+                 ******************************************************************************/
+                updateRecyclerView(getTestData2());
+            }
+        });
+    }
+
+    private void updateRecyclerView(List<DailyAttendanceReportItem> items){
+        myAdapter.changeItems(items);
     }
 
     @Override
@@ -87,45 +141,93 @@ public class DailyAttendanceReportFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /*******************************************************************************
+
+     TODO: 작업1 - 서버에서 데이터 불러오는 작업 필요 By 날짜 (REST API 개발중)
+     날짜로 데이터 불러오는 REST API 필요
+
+     ******************************************************************************/
     private List<DailyAttendanceReportItem> getTestData() {
         ArrayList<DailyAttendanceReportItem> list = new ArrayList<>();
 
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("정영훈",
+                "개발팀", "201000000", "근무중", "09:00", "17:00", "09:00", null));
+        list.add(new DailyAttendanceReportItem("천재웅",
+                "개발팀", "201000000", "퇴근완료", "10:00", null, "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("최다빈",
+                "개발팀", "201000000", "퇴근완료", "11:00", "17:00", "12:00", "16:00"));
+        list.add(new DailyAttendanceReportItem("김수현",
+                "기획팀", "201739433", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("서예지",
+                "경영팀", "201839413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        return list;
+    }
+    private List<DailyAttendanceReportItem> getTestData2() {
+        ArrayList<DailyAttendanceReportItem> list = new ArrayList<>();
+
+        list.add(new DailyAttendanceReportItem("테스트",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "11:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("정영훈",
+                "개발팀", "201000000", "근무중", "09:00", "17:00", "09:00", null));
+        list.add(new DailyAttendanceReportItem("천재웅",
+                "개발팀", "201000000", "퇴근완료", "10:00", null, "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("최다빈",
+                "개발팀", "201000000", "퇴근완료", "11:00", "17:00", "12:00", "16:00"));
+        list.add(new DailyAttendanceReportItem("김수현",
+                "기획팀", "201739433", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("서예지",
+                "경영팀", "201839413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         list.add(new DailyAttendanceReportItem("김태호",
-                "개발팀", "201739413", "퇴근완료","09:00","17:00","09:00","18:00"));
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
+        list.add(new DailyAttendanceReportItem("김태호",
+                "개발팀", "201739413", "퇴근완료", "09:00", "17:00", "09:00", "18:00"));
         return list;
     }
 
--- app/src/main/java/com/gbridge/etners/ui/admin/adapter/DailyAttendanceReportAdapter.java
+++ app/src/main/java/com/gbridge/etners/ui/admin/adapter/DailyAttendanceReportAdapter.java
@@ -1,29 +1,44 @@
 package com.gbridge.etners.ui.admin.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gbridge.etners.R;
 import com.gbridge.etners.data.DailyAttendanceReportItem;
 import com.jaygoo.widget.RangeSeekBar;
+import com.jaygoo.widget.SeekBar;
 
 
+import java.sql.Array;
 import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 
 public class DailyAttendanceReportAdapter extends RecyclerView.Adapter<DailyAttendanceReportAdapter.ViewHolder> {
 
 
-    private final List<DailyAttendanceReportItem> items;
+    private List<DailyAttendanceReportItem> items;
 
+    public DailyAttendanceReportAdapter() {
+        this.items = new ArrayList<>();
+    }
 
     public DailyAttendanceReportAdapter(List<DailyAttendanceReportItem> items) {
-        this.items = items;
+        this.items = new ArrayList<>();
+        this.items.addAll(items);
     }
 
 
@@ -38,12 +53,122 @@ public class DailyAttendanceReportAdapter extends RecyclerView.Adapter<DailyAtte
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         DailyAttendanceReportItem mItem = items.get(position);
-        holder.rangeSeekBar.setProgress(0,80);
-        holder.rangeSeekBar.getLeftSeekBar().setIndicatorText("09:00 출근");
-        holder.rangeSeekBar.getLeftSeekBar().setIndicatorPaddingLeft(15);
-        holder.rangeSeekBar.getRightSeekBar().setIndicatorText("17:00 퇴근");
-        holder.rangeSeekBar.getRightSeekBar().setIndicatorPaddingRight(15);
-        holder.rangeSeekBar.setEnabled(false);
+
+        holder.tvName.setText(mItem.getName());
+        holder.tvDepartment.setText(mItem.getDepartment());
+        holder.tvEmployeeNumber.setText(mItem.getEmployeeNumber());
+
+
+        /*******************************************************************************
+
+         TODO: 작업2 : 근무상태 목록 정하고 변경 필요
+
+         ******************************************************************************/
+        String status = mItem.getStatus();
+        switch (status) {
+            case "출근":
+                holder.tvStatus.setBackgroundResource(R.drawable.bg_round_a39ef1_view);
+                break;
+            case "퇴근":
+                holder.tvStatus.setBackgroundResource(R.drawable.bg_round_00c8ff_view);
+                break;
+            case "근무":
+                holder.tvStatus.setBackgroundResource(R.drawable.bg_round_8dc100_view);
+                break;
+            default:
+                holder.tvStatus.setBackgroundResource(R.drawable.bg_round_000181_view);
+        }
+        holder.tvStatus.setText(status);
+
+
+
+        RangeSeekBar rangeSeekBar = holder.rangeSeekBar;
+        rangeSeekBar.setPadding(20, 0, 20, 0);
+        rangeSeekBar.getLeftSeekBar().setIndicatorPaddingLeft(15);
+        rangeSeekBar.getRightSeekBar().setIndicatorPaddingRight(15);
+        rangeSeekBar.setEnabled(false);
+
+
+        String startTime = mItem.getStartTime();
+        String endTime = mItem.getEndTime();
+
+        if (startTime == null || startTime.isEmpty() || endTime == null || endTime.isEmpty()) {
+            String[] tickArray = new String[5];
+            Arrays.fill(tickArray, "");
+            tickArray[2] = "등록된 근무가 없습니다.";
+            rangeSeekBar.setTickMarkTextArray(tickArray);
+            SeekBar leftSeekBar = rangeSeekBar.getLeftSeekBar();
+            SeekBar rightSeekbar = rangeSeekBar.getRightSeekBar();
+            leftSeekBar.setThumbDrawableId(R.drawable.ic_circle_invisible);
+            rightSeekbar.setThumbDrawableId(R.drawable.ic_circle_invisible);
+            leftSeekBar.setIndicatorTextColor(Color.TRANSPARENT);
+            rightSeekbar.setIndicatorTextColor(Color.TRANSPARENT);
+            return;
+        } else {
+            String[] tickArray = new String[2];
+            tickArray[0] = startTime;
+            tickArray[1] = endTime;
+            rangeSeekBar.setTickMarkTextArray(tickArray);
+        }
+
+
+        String clockInTime = mItem.getClockInTime();
+        String clockOutTIme = mItem.getClockOutTime();
+
+        if (clockInTime == null || clockInTime.isEmpty()) {
+            SeekBar leftSeekBar = rangeSeekBar.getLeftSeekBar();
+            SeekBar rightSeekbar = rangeSeekBar.getRightSeekBar();
+            leftSeekBar.setThumbDrawableId(R.drawable.ic_circle_invisible);
+            rightSeekbar.setThumbDrawableId(R.drawable.ic_circle_invisible);
+            leftSeekBar.setIndicatorTextColor(Color.TRANSPARENT);
+            rightSeekbar.setIndicatorTextColor(Color.TRANSPARENT);
+
+        } else if (clockOutTIme == null || clockOutTIme.isEmpty()) {
+            SeekBar rightSeekbar = rangeSeekBar.getRightSeekBar();
+            rightSeekbar.setThumbDrawableId(R.drawable.ic_circle_invisible);
+            rightSeekbar.setIndicatorTextColor(Color.TRANSPARENT);
+            rangeSeekBar.setProgress(0,0);
+            rangeSeekBar.getLeftSeekBar().setIndicatorText(mItem.getClockInTime() + " 출근");
+        } else {
+            float leftSeekBarPercent = 0;
+            float rightSeekBarPercent = 100;
+            try {
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.KOREA);
+                Long longStartTime = timeFormat.parse(mItem.getStartTime()).getTime();
+                Long longEndTime = timeFormat.parse(mItem.getEndTime()).getTime();
+                Long longClockInTime = timeFormat.parse(mItem.getClockInTime()).getTime();
+                Long longClockOutTime = timeFormat.parse(mItem.getClockOutTime()).getTime();
+                long total = longEndTime - longStartTime;
+
+                if (longClockInTime > longStartTime) {
+                    long left = longClockInTime - longStartTime;
+                    leftSeekBarPercent = left * 100f / total;
+                }
+
+                if (longClockOutTime < longEndTime) {
+                    long right = longClockOutTime - longStartTime;
+                    rightSeekBarPercent = right * 100f / total;
+                }
+
+
+            } catch (ParseException | NullPointerException e) {
+                e.printStackTrace();
+            }
+
+            rangeSeekBar.setProgress(leftSeekBarPercent, rightSeekBarPercent);
+            rangeSeekBar.getLeftSeekBar().setIndicatorText(mItem.getClockInTime() + " 출근");
+            rangeSeekBar.getRightSeekBar().setIndicatorText(mItem.getClockOutTime() + " 퇴근");
+        }
+
+
+    }
+
+    public void changeItems(List<DailyAttendanceReportItem> items) {
+        if (this.items != null) {
+            this.items.clear();
+        }
+        this.items.addAll(items);
+        notifyDataSetChanged();
     }
 
 
@@ -55,22 +180,22 @@ public class DailyAttendanceReportAdapter extends RecyclerView.Adapter<DailyAtte
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        RangeSeekBar rangeSeekBar;
+        final TextView tvName;
+        final TextView tvDepartment;
+        final TextView tvEmployeeNumber;
+        final TextView tvStatus;
+        final RangeSeekBar rangeSeekBar;
+
         public ViewHolder(View view) {
             super(view);
             rangeSeekBar = view.findViewById(R.id.seek_bar_item_daily_attendance_report);
-
+            tvName = view.findViewById(R.id.tv_daily_attendance_report_name);
+            tvDepartment = view.findViewById(R.id.tv_daily_attendance_report_department);
+            tvEmployeeNumber = view.findViewById(R.id.tv_daily_attendance_report_employee_number);
+            tvStatus = view.findViewById(R.id.tv_daily_attendance_report_status);
 
         }
     }
 
 
-    private class myRangeSeekbar extends RangeSeekBar{
-
-        public myRangeSeekbar(Context context) {
-            super(context);
-        }
-
-
-    }
 }
